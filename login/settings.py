70c2feb09e29c9a1c874a96b895a4ded5eef4c9f
Blerdijan <blerdijankoliqi@gmail.com>
2021-03-07 19:39:24 +0100

    koment

--- login/settings.py
+++ login/settings.py
@@ -95,10 +95,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 import dj_database_url
 db_from_env= dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+'''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
