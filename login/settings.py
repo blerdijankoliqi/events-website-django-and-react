98ca7bc30c36416ca22944849b0a5c2f84722046
Blerdijan <blerdijankoliqi@gmail.com>
2021-03-07 18:48:01 +0100

    i hope it works2

--- login/settings.py
+++ login/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
