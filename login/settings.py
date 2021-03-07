c54edcbd04e0746087a08d9fdf0e25c98e6be8e9
Blerdijan <blerdijankoliqi@gmail.com>
2021-03-07 23:31:07 +0100

    me bismilah

--- login/settings.py
+++ login/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '4#*j!_kw^4y2+b)to_fa=*nsza43)c87t&6l6id)z%hefl$4jx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoteam.herokuapp.com','127.0.0.1:8000']
+
 
 
 # Application definition
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,12 +90,14 @@ WSGI_APPLICATION = 'login.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'festival_kulture',
-        'USER': 'database_adminuser',
-        'PASSWORD': 'cGAO04rvTvrasdafas324324TgSmJ66G6LkHgJeHm1o',
+        'NAME': 'final',
+        'USER': 'postgres',
+        'PASSWORD': '123',
         'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -101,6 +105,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangoteam7@gmail.com'
 EMAIL_HOST_PASSWORD = 'pffpcqukeylzakji'
 EMAIL_USE_TLS = True
+
+
 
 
 # Password validation
@@ -140,6 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
